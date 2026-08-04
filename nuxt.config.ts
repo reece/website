d1382b673bb14fba04d126f07aab20db9a027b13
Reece Hart <reecehart@gmail.com>
2026-08-04 12:10:00 -0700

    feat: add site favicon
    Serves public/favicon.ico and links it explicitly in the document
    head for browsers that don't auto-probe /favicon.ico.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-11-01',
 
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+
   modules: [
     '@nuxt/content',
     '@unocss/nuxt',
